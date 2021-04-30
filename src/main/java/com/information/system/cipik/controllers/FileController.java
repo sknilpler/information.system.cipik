cff7b6b34525dde9d6d7691bd390d0f47036bd17
first <nesterov_y92@mail.ru>
2021-04-30 13:53:54 +0500

    adding statistic info and add function to print employee

--- src/main/java/com/information/system/cipik/controllers/FileController.java
+++ src/main/java/com/information/system/cipik/controllers/FileController.java
@@ -1,24 +1,36 @@
 package com.information.system.cipik.controllers;
 
+import com.information.system.cipik.models.Employee;
+import com.information.system.cipik.models.IPMStandard;
+import com.information.system.cipik.repo.EmployeeRepository;
+import com.information.system.cipik.repo.IPMStandardRepository;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class FileController {
@@ -28,6 +40,11 @@ public class FileController {
     private String dbUserName = "craft";
     private String dbUserPassword = "111";
     private String dbNameList = "cipik";
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    IPMStandardRepository ipmStandardRepository;
 
     /**
      * Выгрузка SQL дампа БД пользователю
@@ -153,6 +170,114 @@ public class FileController {
         }
 
         return "redirect:/admin";
+    }
+
+
+    @GetMapping("/userPage/employee-siz/print-personal-card/{id}")
+    public void printEmployeePersonalCard (@PathVariable(value = "id") long id, HttpServletResponse response) throws IOException {
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=lichnaya_kartochka_"+employee.getSurname()+"_"+employee.getName()+"_"+employee.getPatronymic()+".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        String excelFilePath = Paths.get("").toAbsolutePath().toString()+File.separator+"template"+File.separator+"Личная карточка.xlsx";
+
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+            Workbook workbook = WorkbookFactory.create(inputStream);
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            Cell cellSurname = sheet.getRow(6).getCell(1);
+            Cell cellName = sheet.getRow(7).getCell(1);
+            Cell cellPatronymic = sheet.getRow(7).getCell(4);
+            Cell cellTabnom = sheet.getRow(8).getCell(3);
+            Cell cellKomplex = sheet.getRow(9).getCell(3);
+            Cell cellPost = sheet.getRow(10).getCell(2);
+
+            Cell cellDateStartWork = sheet.getRow(11).getCell(2);
+            Cell cellSex = sheet.getRow(6).getCell(8);
+            Cell cellHeight = sheet.getRow(7).getCell(8);
+            Cell cellSizeWear = sheet.getRow(9).getCell(8);
+            Cell cellSizeShoe = sheet.getRow(10).getCell(8);
+            Cell cellSizeHead = sheet.getRow(11).getCell(9);
+            Cell cellSizeGasMask = sheet.getRow(12).getCell(9);
+            Cell cellSizeRespirator = sheet.getRow(13).getCell(9);
+            Cell cellSizeMittens = sheet.getRow(14).getCell(9);
+            Cell cellSizeGlove = sheet.getRow(15).getCell(9);
+
+            cellSurname.setCellValue(employee.getSurname());
+            cellName.setCellValue(employee.getName());
+            cellPatronymic.setCellValue(employee.getPatronymic());
+            cellTabnom.setCellValue(employee.getTabNomer());
+            cellKomplex.setCellValue(employee.getKomplex().getShortName());
+            cellPost.setCellValue(employee.getPost().getPostName());
+            DateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
+            cellDateStartWork.setCellValue(dateFormatter.format(employee.getDataStartWork()));
+            cellSex.setCellValue(employee.getSex());
+            cellHeight.setCellValue(employee.getHeight());
+            cellSizeWear.setCellValue(employee.getClothingSize());
+            cellSizeShoe.setCellValue(employee.getShoeSize());
+            cellSizeHead.setCellValue(employee.getHeadgearSize());
+            cellSizeGasMask.setCellValue(employee.getGasMaskSize());
+            cellSizeRespirator.setCellValue(employee.getRespiratorSize());
+            cellSizeMittens.setCellValue(employee.getMittensSize());
+            cellSizeGlove.setCellValue(employee.getGloveSize());
+
+            int row = 19;
+            Row exampleRow = sheet.getRow(19);
+            List<IPMStandard> standards = ipmStandardRepository.findAllByPostId(employee.getPost().getId());
+            for (IPMStandard s: standards) {
+                sheet.getRow(row).setRowStyle(exampleRow.getRowStyle());
+                sheet.getRow(row).setHeight(exampleRow.getHeight());
+                sheet.addMergedRegion(new CellRangeAddress(
+                        row+1, //first row (0-based)
+                        row+1, //last row  (0-based)
+                        0, //first column (0-based)
+                        3  //last column  (0-based)
+                ));
+                sheet.addMergedRegion(new CellRangeAddress(
+                        row+1, //first row (0-based)
+                        row+1, //last row  (0-based)
+                        4, //first column (0-based)
+                        5  //last column  (0-based)
+                ));
+                sheet.addMergedRegion(new CellRangeAddress(
+                        row+1, //first row (0-based)
+                        row+1, //last row  (0-based)
+                        6, //first column (0-based)
+                        7  //last column  (0-based)
+                ));
+                sheet.addMergedRegion(new CellRangeAddress(
+                        row+1, //first row (0-based)
+                        row+1, //last row  (0-based)
+                        8, //first column (0-based)
+                        9  //last column  (0-based)
+                ));
+
+                sheet.getRow(row).getCell(0).setCellValue(s.getIndividualProtectionMeans().getNameSIZ());
+                sheet.getRow(row).getCell(0).setCellStyle(exampleRow.getCell(0).getCellStyle());
+                sheet.getRow(row).getCell(4).setCellStyle(exampleRow.getCell(4).getCellStyle());
+                sheet.getRow(row).getCell(6).setCellValue(s.getIndividualProtectionMeans().getEd_izm());
+                sheet.getRow(row).getCell(6).setCellStyle(exampleRow.getCell(6).getCellStyle());
+                sheet.getRow(row).getCell(8).setCellValue(s.getIssuanceRate() + " на "+s.getServiceLife() + " мес");
+                sheet.getRow(row).getCell(8).setCellStyle(exampleRow.getCell(8).getCellStyle());
+                sheet.getRow(row).getCell(9).setCellStyle(exampleRow.getCell(9).getCellStyle());
+                row++;
+            }
+
+            inputStream.close();
+
+            ServletOutputStream outputStream = response.getOutputStream();
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+
+        } catch (IOException | EncryptedDocumentException
+                 ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
