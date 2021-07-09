723ad7beba7f41800241ae20c8f56d5f71577f44
first <nesterov_y92@mail.ru>
2021-07-09 15:27:31 +0500

    add printing issued items to komplex

--- src/main/java/com/information/system/cipik/controllers/ItemController.java
+++ src/main/java/com/information/system/cipik/controllers/ItemController.java
@@ -4,14 +4,24 @@ package com.information.system.cipik.controllers;
 import com.information.system.cipik.models.*;
 import com.information.system.cipik.repo.*;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -216,9 +226,12 @@ public class ItemController {
     /*
      * Сохранение и печать выданных МЦ
      */
-    @PostMapping("/userPage/add-coming-for-item/send/{id}")
-    public void saveIssuedItem(@RequestBody Map<Long,String> objectList, HttpServletResponse response, @RequestParam String employeeAccepting,
-                               @RequestParam String employeeGaveOut, @RequestParam String dateIssued, @RequestParam String invoiceNumber,
+    @PostMapping("/userPage/add-coming-for-item/send/{id}/{employeeAccepting}/{employeeGaveOut}/{dateIssued}/{invoiceNumber}")
+    public void saveIssuedItem(@RequestBody Map<String,List<String>> listOfObjects, HttpServletResponse response,
+                               @PathVariable(value ="employeeAccepting") String employeeAccepting,
+                               @PathVariable(value ="employeeGaveOut") String employeeGaveOut,
+                               @PathVariable(value ="dateIssued") String dateIssued,
+                               @PathVariable(value ="invoiceNumber") String invoiceNumber,
                                @PathVariable(value = "id") long id){
         Komplex komplex = komplexRepository.findById(id).orElseThrow();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -229,12 +242,82 @@ public class ItemController {
             // e.printStackTrace();
             System.out.println("Не удалось преобразовать дату полученную от клиента, будет установлена текущая дата");
         }
-        for (Map.Entry<Long,String> entry : objectList.entrySet()) {
-            Item item = itemsRepository.findById(entry.getKey()).orElseThrow();
-            double num_new = Double.parseDouble(entry.getValue());
+        System.out.println(listOfObjects.size());
+        List<Item> items = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : listOfObjects.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue().get(0));
+            Item item = itemsRepository.findById(Long.parseLong(entry.getKey())).orElseThrow();
+            double num_new = Double.parseDouble(entry.getValue().get(0));
             item.setNumber(item.getNumber()-num_new);
             itemsRepository.save(item);
             issuanceItemsRepository.save(new IssuanceItems(num_new+"",dateI,invoiceNumber,employeeAccepting,employeeGaveOut,item,komplex));
+            item.setNumber(num_new);
+            items.add(item);
+        }
+
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+
+        String headerValue = "attachment; filename=nakladnaya_vidachi_mc_podrazdeleniyu.xls";
+        response.setHeader(headerKey, headerValue);
+
+        String excelFilePath = Paths.get("").toAbsolutePath().toString() + File.separator + "template" + File.separator + "шаблон накладная выдачи МЦ.xls";
+
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+            Workbook workbook = WorkbookFactory.create(inputStream);
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            sheet.getRow(3).getCell(0).setCellValue("ТРЕБОВАНИЕ-НАКЛАДНАЯ №"+invoiceNumber);
+            SimpleDateFormat format2 = new SimpleDateFormat("dd.MM.yyyy");
+            sheet.getRow(9).getCell(0).setCellValue(format2.format(dateI));
+            sheet.getRow(9).getCell(4).setCellValue("ЦИП ИК      "+komplex.getShortName());
+
+            int row = 17;
+            Row exampleRow = sheet.getRow(17);
+            int count = 1;
+            for (Item it : items) {
+                if (it.getNumber() != 0) {
+                    sheet.getRow(row).setRowStyle(exampleRow.getRowStyle());
+                    sheet.getRow(row).setHeight(exampleRow.getHeight());
+                    if (row>17){
+                    sheet.addMergedRegion(new CellRangeAddress(row,row,2,3));}
+                    for (int i = 0; i < 12; i++) {
+                        sheet.getRow(row).getCell(i).setCellStyle(exampleRow.getCell(i).getCellStyle());
+                    }
+                    sheet.getRow(row).getCell(0).setCellValue(it.getBill());
+                   // sheet.addMergedRegion(new CellRangeAddress(row,row,2,3));
+                    sheet.getRow(row).getCell(2).setCellValue(it.getName());
+                    sheet.getRow(row).getCell(4).setCellValue(it.getNomenclature());
+                    sheet.getRow(row).getCell(5).setCellValue(it.getCode());
+                    sheet.getRow(row).getCell(6).setCellValue(it.getUnit());
+                    sheet.getRow(row).getCell(7).setCellValue(it.getNumber());
+                    sheet.getRow(row).getCell(8).setCellValue(it.getNumber());
+                    row++;
+                    count++;
+                }
+            }
+
+//            Row exampleRow2 = sheet.getRow(2);
+//            for (int i = 0; i < 12; i++) {
+//                sheet.getRow(19+items.size()).getCell(i).setCellStyle();
+//            }
+//            sheet.getRow(19+items.size()).getCell(0).setCellValue("Отпустил ");
+//            sheet.getRow(19+items.size()).getCell(3).setCellValue(employeeGaveOut);
+//            sheet.getRow(19+items.size()).getCell(4).setCellValue("Получил ");
+//            sheet.getRow(19+items.size()).getCell(11).setCellValue(employeeAccepting);
+
+            inputStream.close();
+
+            ServletOutputStream outputStream = response.getOutputStream();
+            workbook.write(outputStream);
+            workbook.close();
+            outputStream.close();
+
+        } catch (IOException | EncryptedDocumentException
+                ex) {
+            ex.printStackTrace();
         }
     }
 
